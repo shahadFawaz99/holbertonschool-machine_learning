18091e4f41e271daafb220aeac7fd7453cf472c9
shahadFawaz99 <shahadfwaz2@gmail.com>
2026-02-21 23:27:21 +0300

    task2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module that defines matrix_shape"""
 def matrix_shape(matrix):
-    shape = []
-    while isinstance(matrix, list):
-        shape.append(len(matrix))
-        if len(matrix) == 0:
-            break
-        matrix = matrix[0]
-    return shape
+"""Returns the shape of a matrix as a list of integers"""
+shape = []
+while isinstance(matrix, list):
+shape.append(len(matrix))
+if len(matrix) == 0:
+break
+matrix = matrix[0]
+return shape
