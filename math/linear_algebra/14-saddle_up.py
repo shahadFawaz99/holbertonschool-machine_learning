37cdc97e8f0f1f9f42b2dd2c50786e9c8a0da905
shahadFawaz99 <shahadfwaz2@gmail.com>
2026-02-21 23:54:49 +0300

    task13

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that defines np_matmul"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication using numpy"""
+    return np.matmul(mat1, mat2)
