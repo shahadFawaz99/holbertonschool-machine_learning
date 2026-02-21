0bbd0a4a56d443c02481d52526c5e79b539355f9
shahadFawaz99 <shahadfwaz2@gmail.com>
2026-02-21 23:44:44 +0300

    task10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that defines np_shape"""
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray as a tuple"""
+    return matrix.shape
