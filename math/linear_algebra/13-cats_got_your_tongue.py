b7c1dc046c7926556f92d7584d99cf1cc4cde9a7
shahadFawaz99 <shahadfwaz2@gmail.com>
2026-02-21 23:53:23 +0300

    task13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module that defines np_cat"""
 
+import numpy as np
+
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenates two matrices along a specific axis"""
-    np = __import__('numpy')
     return np.concatenate((mat1, mat2), axis=axis)
