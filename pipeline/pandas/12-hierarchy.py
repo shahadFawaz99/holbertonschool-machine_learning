800713e3de9184be4605aa6e14e275637acda5c9
shahadFawaz99 <shahadfwaz2@gmail.com>
2026-03-31 18:29:27 +0300

    task12

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
-"""now we will use concatination"""
-
-
+"""Module that concatenates two DataFrames with a MultiIndex,
+with Timestamp first."""
 import pandas as pd
 index = __import__('10-index').index
 
 
-def concat(df1, df2):
-    """we will set indexes of both dfs and then concatenate"""
+def hierarchy(df1, df2):
+    """Concatenate bitstamp and coinbase DataFrames with Timestamp
+    as first index level for timestamps 1417411980 to 1417417980,
+    inclusive.
+
+    Args:
+        df1: pandas DataFrame (coinbase)
+        df2: pandas DataFrame (bitstamp)
+
+    Returns:
+        pandas.DataFrame: concatenated DataFrame with MultiIndex
+        [Timestamp, source]
+    """
     df1 = index(df1)
     df2 = index(df2)
-    df2 = df2.loc[:1417411920]
-    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-    return df
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
+    df_concat = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    df_concat = df_concat.swaplevel(0, 1)
+    df_concat = df_concat.sort_index(level=0)
+    return df_concat
